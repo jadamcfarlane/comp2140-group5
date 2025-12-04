e663ce5254d03b0dfc5e0e431bbe9260c6b151d1
jadamcfarlane <jadamcfarlane5@gmail.com>
2025-12-03 23:08:15 -0500

    Add files via upload

---
+++ UserActivity.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UserActivity.aspx.cs" Inherits="Aquasol.UserActivity" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>User Activity Report</title>
+    <link href="UserActivity.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <header>
+            <ul class="navigation">
+                <li><a href="MPOS.aspx">Dashboard</a></li>
+                <li><a href="Report.aspx">Reports</a></li>
+                <li><a href="LoginPage.aspx">Sign Out</a></li>
+            </ul>
+</header>
+        <div class="main">
+            <h1>User Activity Report</h1>
+
+            <div class="filter-section">
+                <asp:Label ID="lblUser" runat="server" Text="User: "></asp:Label>
+                <asp:DropDownList ID="ddlUsers" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlUsers_SelectedIndexChanged"></asp:DropDownList>
+
+                <asp:Label ID="lblStart" runat="server" Text="Start Date: "></asp:Label>
+                <asp:TextBox ID="txtStartDate" runat="server" CssClass="date-input"></asp:TextBox>
+
+                <asp:Label ID="lblEnd" runat="server" Text="End Date: "></asp:Label>
+                <asp:TextBox ID="txtEndDate" runat="server" CssClass="date-input"></asp:TextBox>
+
+                <asp:Button ID="btnFilter" runat="server" Text="Filter" CssClass="btnsubmit" OnClick="btnFilter_Click" />
+                <asp:Button ID="btnRefresh" runat="server" Text="Refresh" CssClass="btnsubmit" OnClick="btnRefresh_Click" />
+            </div>
+
+            <asp:GridView ID="gvUserActivity" runat="server" AutoGenerateColumns="False" CssClass="grid-items">
+                <Columns>
+                    <asp:BoundField DataField="ActivityTime" HeaderText="Time" DataFormatString="{0:yyyy-MM-dd HH:mm:ss}" />
+                    <asp:BoundField DataField="UserName" HeaderText="User" />
+                    <asp:BoundField DataField="ActivityType" HeaderText="Activity Type" />
+                    <asp:BoundField DataField="ActivityDescription" HeaderText="Description" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ UserActivity.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aquasol
+{
+    public partial class UserActivity : System.Web.UI.Page
+    {
+        private string connStr = @"Server=DESKTOP-KIRN4D4\SQLEXPRESS;Database=Aquasol;Trusted_Connection=True;";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                txtStartDate.Text = DateTime.Now.AddDays(-7).ToString("yyyy-MM-dd");
+                txtEndDate.Text = DateTime.Now.ToString("yyyy-MM-dd");
+                LoadUsersDropdown();
+                LoadActivityReport();
+            }
+        }
+
+        private void LoadUsersDropdown()
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT UserID, UserName FROM Users ORDER BY UserName", conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                ddlUsers.DataSource = dt;
+                ddlUsers.DataTextField = "UserName";
+                ddlUsers.DataValueField = "UserID";
+                ddlUsers.DataBind();
+
+                ddlUsers.Items.Insert(0, new ListItem("All Users", "0"));
+            }
+        }
+
+        private void LoadActivityReport()
+        {
+            int selectedUserId = int.Parse(ddlUsers.SelectedValue);
+            DateTime startDate = DateTime.Parse(txtStartDate.Text);
+            DateTime endDate = DateTime.Parse(txtEndDate.Text).AddDays(1).AddSeconds(-1); // include entire day
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                string query = @"
+            SELECT ua.ActivityTime, u.UserName, ua.ActivityType, ua.ActivityDescription
+            FROM UserActivity ua
+            INNER JOIN Users u ON ua.UserID = u.UserID
+            WHERE ua.ActivityTime BETWEEN @start AND @end";
+
+                if (selectedUserId > 0)
+                {
+                    query += " AND ua.UserID = @userId";
+                }
+
+                query += " ORDER BY ua.ActivityTime DESC";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@start", startDate);
+                cmd.Parameters.AddWithValue("@end", endDate);
+                if (selectedUserId > 0)
+                    cmd.Parameters.AddWithValue("@userId", selectedUserId);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                gvUserActivity.DataSource = dt;
+                gvUserActivity.DataBind();
+            }
+        }
+
+        protected void ddlUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadActivityReport();
+        }
+
+        protected void btnFilter_Click(object sender, EventArgs e)
+        {
+            LoadActivityReport();
+        }
+
+        protected void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadActivityReport();
+        }
+    }
+}

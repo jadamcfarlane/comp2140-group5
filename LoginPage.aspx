2bee283ee2fc216264b288dfc1ade051d52c0e34
jadamcfarlane <jadamcfarlane5@gmail.com>
2025-12-03 23:10:24 -0500

    Add files via upload

---
+++ LoginPage.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="LoginPage.aspx.cs" Inherits="Aquasol.LoginPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+        <title>LOGIN</title>
+        <link href="LoginPage.css" rel="stylesheet" />
+
+</head>
+<body>
+    <form id="form1" runat="server">
+
+    <header>
+        <img src="logo.jpg" style="height: 194px; width: 285px" /><br />
+        <br />
+    </header>
+
+    <div class="loginbox"> 
+        <h1>MSOL JAMAICA LTD</h1>
+        <h3>AQUASOL</h3>
+        <h2>Login Here</h2>
+        <asp:Label Text ="Username" CssClass="lblemail" runat="server"  />
+        <asp:TextBox  CssClass="txtemail" runat="server" ID="usern" placeholder="Please enter your email address."/>
+        <asp:Label Text ="Password" CssClass="lblpassword" runat="server"  />
+        <asp:TextBox CssClass="txtpassword" runat="server" ID="pass" placeholder="Please enter your password." />
+        <asp:Button ID="loginButton1" runat="server" Text="LOGIN" CssClass="btnsubmit" OnClick="loginButton1_Click" />
+        <br />
+        <br />
+        <asp:LinkButton Text="Forget Password" CssClass="btnforget" runat="server" />
+        <br />
+                
+    </div>
+    </form>
+</body>
+</html>
---
+++ LoginPage.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aquasol
+{
+    public partial class LoginPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void loginButton1_Click(object sender, EventArgs e)
+        {
+
+            string username = usern.Text.Trim();
+            string password = pass.Text.Trim();
+
+            if (username == "" || password == "")
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Enter username and password');", true);
+                return;
+            }
+
+            string connStr = @"Server=DESKTOP-KIRN4D4\SQLEXPRESS;Database=Aquasol;Trusted_Connection=True;";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                string sql = "SELECT UserID, Role FROM Users WHERE UserName=@u AND Password=@p";
+
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@u", username);
+                cmd.Parameters.AddWithValue("@p", password);
+
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    int userID = Convert.ToInt32(reader["UserID"]);
+                    string role = reader["Role"].ToString();
+
+                    ActivityLogger.Log(userID, "Login", "User logged in successfully.");
+
+                    if (role == "Admin")
+                    {
+                        Response.Redirect("MPOS.aspx");   
+                    }
+                    else if (role == "User")
+                    {
+                        Response.Redirect("UPOS.aspx");   
+                    }
+                    else
+                    {
+                        ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Unknown role');", true);
+                    }
+                }
+                else
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Invalid login');", true);
+                }
+
+                reader.Close();
+            }
+        }
+    }
+}

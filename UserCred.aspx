7adea4adf039fda875dc38cb54a3981868b5c2a8
jadamcfarlane <jadamcfarlane5@gmail.com>
2025-12-03 22:59:38 -0500

    Add files via upload

---
+++ UserCred.aspx
@@ -0,0 +1,76 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UserCred.aspx.cs" Inherits="Aquasol.UserCred" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>User Credential</title>
+    <link href="UserCred.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <header>
+            <ul class="navigation">
+                <li><a href="MPOS.aspx">Dashboard</a></li>
+                <li><a href="LoginPage.aspx">Sign Out</a></li>
+            </ul>
+        </header>
+        <div class="user-container">
+
+            <h2>User Credentials</h2>
+
+            <div class="form-group">
+                <label>User Name</label>
+                <asp:TextBox ID="txtID" runat="server" CssClass="input"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server"
+                    ControlToValidate="txtID" CssClass="error" ErrorMessage="Please Enter a valid User ID" />
+            </div>
+
+            <div class="form-group">
+                <label>First Name</label>
+                <asp:TextBox ID="Fname" runat="server" CssClass="input"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server"
+                    ControlToValidate="Fname" CssClass="error" ErrorMessage="Please Fill out field" />
+            </div>
+
+            <div class="form-group">
+                <label>Last Name</label>
+                <asp:TextBox ID="Lname" runat="server" CssClass="input"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server"
+                    ControlToValidate="Lname" CssClass="error" ErrorMessage="Please Fill out Field" />
+            </div>
+
+            <div class="form-group">
+                <label>Password</label>
+                <asp:TextBox ID="Passw" runat="server" TextMode="Password" CssClass="input"></asp:TextBox>
+            </div>
+
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <asp:TextBox ID="password" runat="server" TextMode="Password" CssClass="input"></asp:TextBox>
+                <asp:CompareValidator ID="CompareValidator1" runat="server"
+                    ControlToCompare="Passw" ControlToValidate="password"
+                    CssClass="error" ErrorMessage="Passwords Do Not Match" />
+            </div>
+
+            <div class="form-group">
+                <label>User Type</label>
+                <asp:DropDownList ID="DropDownList1" runat="server" CssClass="input">
+                    <asp:ListItem>User</asp:ListItem>
+                    <asp:ListItem>Admin</asp:ListItem>
+                </asp:DropDownList>
+            </div>
+
+            <div class="btn-row">
+                <asp:Button ID="UpdateUser" runat="server" CssClass="btnsubmit" Text="Save Changes" OnClick="UpdateUser_Click1"  />
+                <asp:Button ID="DeleteUser" runat="server" CssClass="btnsubmit danger" Text="Delete User" OnClick="DeleteUser_Click1"  />
+                <asp:Button ID="Cancel" runat="server" CssClass="btnsubmit" Text="Cancel" OnClick="Cancel_Click"  />
+            </div>
+
+            <asp:Label ID="LblMess" runat="server" CssClass="message"></asp:Label>
+
+        </div>
+    </form>
+</body>
+
+</html>
---
+++ UserCred.aspx.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aquasol
+{
+    public partial class UserCred : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection(@"Server=DESKTOP-KIRN4D4\SQLEXPRESS;Database=Aquasol;Trusted_Connection=True;");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (!IsPostBack)
+            {
+                ClearFunction();
+            }
+        }
+
+        private void LoadUser(string userId)
+        {
+            if (string.IsNullOrWhiteSpace(userId))
+                return;
+
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(
+                "SELECT FirstName, LastName, Password, Role FROM Users WHERE UserName = @ID",
+                conn
+            );
+            cmd.Parameters.AddWithValue("@ID", userId);
+
+            SqlDataReader sdr = cmd.ExecuteReader();
+
+            if (sdr.Read())
+            {
+                txtID.Text = userId;
+                Fname.Text = sdr["FirstName"].ToString();
+                Lname.Text = sdr["LastName"].ToString();
+                Passw.Text = sdr["Password"].ToString();
+                password.Text = sdr["Password"].ToString();
+                DropDownList1.SelectedValue = sdr["Role"].ToString();
+                LblMess.Text = "";
+            }
+            else
+            {
+                LblMess.Text = "User not found.";
+            }
+
+            conn.Close();
+        }
+        protected void UpdateUser_Click1(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            
+            SqlCommand checkCmd = new SqlCommand("SELECT UserID FROM Users WHERE UserName=@UserName", conn);
+            checkCmd.Parameters.AddWithValue("@UserName", txtID.Text);
+            object userIdObj = checkCmd.ExecuteScalar();
+
+            int userId;
+
+            if (userIdObj != null) 
+            {
+                userId = (int)userIdObj;
+
+                SqlCommand updateUsers = new SqlCommand(
+                    "UPDATE Users SET Password=@Password, Role=@Role WHERE UserID=@UserID", conn);
+                updateUsers.Parameters.AddWithValue("@Password", Passw.Text);
+                updateUsers.Parameters.AddWithValue("@Role", DropDownList1.SelectedValue);
+                updateUsers.Parameters.AddWithValue("@UserID", userId);
+                updateUsers.ExecuteNonQuery();
+
+                SqlCommand updateCreate = new SqlCommand(
+                    "UPDATE UserCreate SET FirstName=@FirstName, LastName=@LastName WHERE UserID=@UserID", conn);
+                updateCreate.Parameters.AddWithValue("@FirstName", Fname.Text);
+                updateCreate.Parameters.AddWithValue("@LastName", Lname.Text);
+                updateCreate.Parameters.AddWithValue("@UserID", userId);
+                updateCreate.ExecuteNonQuery();
+            }
+            else 
+            {
+                SqlCommand insertUsers = new SqlCommand(
+                    "INSERT INTO Users (UserName, Password, Role) OUTPUT INSERTED.UserID VALUES (@UserName, @Password, @Role)", conn);
+                insertUsers.Parameters.AddWithValue("@UserName", txtID.Text);
+                insertUsers.Parameters.AddWithValue("@Password", Passw.Text);
+                insertUsers.Parameters.AddWithValue("@Role", DropDownList1.SelectedValue);
+
+                userId = (int)insertUsers.ExecuteScalar(); // get new UserID
+
+                SqlCommand insertCreate = new SqlCommand(
+                    "INSERT INTO UserCreate (UserID, FirstName, LastName) VALUES (@UserID, @FirstName, @LastName)", conn);
+                insertCreate.Parameters.AddWithValue("@UserID", userId);
+                insertCreate.Parameters.AddWithValue("@FirstName", Fname.Text);
+                insertCreate.Parameters.AddWithValue("@LastName", Lname.Text);
+                insertCreate.ExecuteNonQuery();
+            }
+
+            LblMess.Text = "User saved successfully!";
+            ClearFunction();
+}
+
+          protected void DeleteUser_Click1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtID.Text))
+            {
+                LblMess.Text = "Enter a UserName to delete.";
+                return;
+            }
+
+            try
+            {
+                conn.Open();
+
+                SqlCommand deleteUser = new SqlCommand("DELETE FROM Users WHERE UserName=@UserName", conn);
+                deleteUser.Parameters.AddWithValue("@UserName", txtID.Text);
+                int rows = deleteUser.ExecuteNonQuery();
+
+                if (rows > 0)
+                {
+                    LblMess.Text = "User deleted successfully!";
+                    ClearFunction();
+                }
+                else
+                {
+                    LblMess.Text = "User not found.";
+                }
+            }
+            catch (Exception ex)
+            {
+                LblMess.Text = "Error: " + ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        protected void Cancel_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("MPOS.aspx");
+        }
+
+        protected void ClearFunction()
+        {
+            txtID.Text = "";
+            Fname.Text = "";
+            Lname.Text = "";
+            Passw.Text = "";
+            password.Text = "";
+            DropDownList1.SelectedIndex = 0;
+        }
+
+    }
+}
